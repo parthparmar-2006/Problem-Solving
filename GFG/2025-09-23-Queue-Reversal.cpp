28527cc8318de20d2c804216570054e5b151c38d
Parth Parmar <113754071+parthparmar-2006@users.noreply.github.com>
2025-09-23 12:54:17 +0530

    Create 2025-09-23-Queue-Reversal.cpp

---
+++ GFG/2025-09-23-Queue-Reversal.cpp
@@ -0,0 +1,19 @@
+// Author: Parth Parmar
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void reverseQueue(queue<int> &q) {
+        // code here
+        stack<int> st;
+        while (!q.empty()) {
+            st.push(q.front());
+            q.pop();
+        }
+        while (!st.empty()) {
+            q.push(st.top());
+            st.pop();
+        } 
+    }
+};
