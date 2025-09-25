58bdd3d2513af111dc32fecb5dce6bfdb529333d
Parth Parmar <113754071+parthparmar-2006@users.noreply.github.com>
2025-09-26 00:13:01 +0530

    Create 2025-09-26-Rotate-Deque-by-K.cpp

---
+++ GFG/2025-09-26-Rotate-Deque-by-K.cpp
@@ -0,0 +1,23 @@
+// Author: Parth Parmar
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void rotateDeque(deque<int>& dq, int type, int k) {
+        // code here
+        if (type==1) {
+            while (k>0) {
+                dq.push_front(dq.back());
+                dq.pop_back();
+                k--;
+            }
+        } else {
+            while (k>0) {
+                dq.push_back(dq.front());
+                dq.pop_front();
+                k--;
+            }
+        } 
+    }
+};
