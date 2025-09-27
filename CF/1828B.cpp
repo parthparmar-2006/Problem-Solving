4203b00edaacd8ca763b5be5890c8136f8b99492
Parth Parmar <113754071+parthparmar-2006@users.noreply.github.com>
2025-09-27 19:52:09 +0530

    Create 1828B.cpp

---
+++ CF/1828B.cpp
@@ -0,0 +1,26 @@
+// Author: Parth Parmar
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        int g = 0;
+        for (int i = 0; i < n; i++) {
+            g = gcd(g, abs(a[i] - (i + 1)));
+        }
+
+        cout << g << "\n";
+    }
+
+    return 0;
+}
