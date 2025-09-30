879bc9339700586041665d0269940edab604071f
Parth Parmar <113754071+parthparmar-2006@users.noreply.github.com>
2025-09-30 14:22:43 +0530

    Create 2025-09-30-2221-Find-Triangular-Sum-of-an-Array.cpp

---
+++ LC/2025-09-30-2221-Find-Triangular-Sum-of-an-Array.cpp
@@ -0,0 +1,15 @@
+// Author: Parth Parmar
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n = nums.size();
+        while (n>1) {
+            for (int i=1; i<n; i++) {
+                nums[i-1] = (nums[i-1]+nums[i])%10;
+            } n--;
+        } return nums[0];
+    }
+};
